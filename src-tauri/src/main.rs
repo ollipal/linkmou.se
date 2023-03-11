e04874a8227e65a2cdd73406680a97b47221b5ce
Olli Paloviita <olli.paloviita@gmail.com>
2023-03-11 14:19:40 +0200

    Mouse moving with rdev demonstrated

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use rdev::{simulate, Button, EventType, Key, SimulateError, display_size};
+use std::{thread, time};
+
+fn get_display_size() -> (u64, u64) {
+    let (w, h) = display_size().unwrap();
+    assert!(w > 0);
+    assert!(h > 0);
+    (w, h)
+}
+
+fn send(event_type: &EventType) {
+    let delay = time::Duration::from_millis(20);
+    match simulate(event_type) {
+        Ok(()) => (),
+        Err(SimulateError) => {
+            println!("We could not send {:?}", event_type);
+        }
+    }
+    // Let ths OS catchup (at least MacOS)
+    thread::sleep(delay);
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
+    send(&EventType::MouseMove { x: 400.0, y: 400.0 });
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
